af1ec97eeef070ed9b05e27bb94194772b9fc954
Ali Alvarez <alvarezali@hotmail.com>
2022-12-03 12:20:49 -0500

    Dynamic content update

---
+++ frontEnd/src/data.js
@@ -0,0 +1,48 @@
+const products = [{
+    _id: 1,
+    image: 'images/exploring_camping.png',
+    name: 'Exploring camping',
+    category: 'sticker',
+    brand: 'HashSticker',
+    price: 1.25,
+    rating: 4.5,
+    numReviews: 10,
+    countInStock: 45
+},
+{
+    _id: 2,
+    image: 'images/gone_fishing.png',
+    name: 'Gone fishing',
+    category: 'sticker',
+    brand: 'DreamSticker',
+    price: 1.00,
+    rating: 4.75,
+    numReviews: 15,
+    countInStock: 53
+},
+{
+    _id: 3,
+    image: 'images/travel_london.png',
+    name: 'Travel London',
+    category: 'sticker',
+    brand: 'uTravelSticker',
+    price: 1.75,
+    rating: 5,
+    numReviews: 25,
+    countInStock: 15
+},
+{
+    _id: 4,
+    image: 'images/travel_mexicopng',
+    name: 'Travel London',
+    category: 'sticker',
+    brand: 'uTravelSticker',
+    price: 1.55,
+    numReviews: 12,
+    countInStock: 19
+}
+];
+
+console.log(products);
+
+export { products };
